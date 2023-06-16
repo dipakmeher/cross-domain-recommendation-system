3183b788a2c684b5293146b27dd6aa6f66d4863c
dipakmeher <dipakmeher54@gmail.com>
2023-06-16 17:43:53 -0400

    Research CDR

---
+++ emcdr.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 19:39:57 2023
+
+@author: dmeher
+"""
+
+import tensorflow as tf
+
+print(tf.__version__)
+
+
+
+# First commit
+# Define a constant tensor
+# x = tf.constant(2)
+# y = tf.constant(3)
+
+# # Perform tensor operations
+# z = tf.add(x, y)
+
+# # Create a TensorFlow session
+# with tf.Session() as sess:
+#     result = sess.run(z)
+#     print("Result:", result)
+
+
+
+
